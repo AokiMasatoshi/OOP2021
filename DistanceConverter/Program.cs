d11fee7b49f206b7f4d3e093b87ca6b030ae2f0b
青木 将隼 <32145@ojs.ac.jp>
2021-05-21 15:53:04 +0900

    P49 リスト2.4

--- DistanceConverter/Program.cs
+++ DistanceConverter/Program.cs
@@ -10,15 +10,24 @@ namespace DistanceConverter {
             //フィートからメートルへの対応表を出力
 
             if (args.Length >= 1 && args[0] == "-tom") {
-                for (int feet = 1; feet <= 10; feet++) {
-                    double meter = FeetToMeter(feet);
-                    Console.WriteLine("{0}ft = {1:0.0000}m", feet, meter);
-                }
+                PrintFeetToMeterList(1,30);
             } else {
-                for (int meter = 1; meter <= 10; meter++) {
-                    double feet = MeterToFeet(meter);
-                    Console.WriteLine("{0}m = {1:0.0000}ft", meter, feet);
-                }
+                PrintMeterToFeetList(1,30);
+            }
+        }
+
+        
+
+        private static void PrintFeetToMeterList(int start, int stop) {
+            for (int feet = start; feet <= stop; feet++) {
+                double meter = FeetToMeter(feet);
+                Console.WriteLine("{0}ft = {1:0.0000}m", feet, meter);
+            }
+        }
+        private static void PrintMeterToFeetList(int start,int stop) {
+            for (int meter = start; meter <= stop; meter++) {
+                double feet = MeterToFeet(meter);
+                Console.WriteLine("{0}m = {1:0.0000}ft", meter, feet);
             }
         }
 
