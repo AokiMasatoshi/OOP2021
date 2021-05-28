85184b9e309735346054e4644fb1cd03ed868b93
青木 将隼 <32145@ojs.ac.jp>
2021-05-28 15:53:47 +0900

    P79 問題2.1　完成

--- Exercise1/Program.cs
+++ Exercise1/Program.cs
@@ -6,9 +6,20 @@ using System.Threading.Tasks;
 
 namespace Exercise1 {
     class Program {
-    
+        static Song[] Songs = new Song[1];
 
         static void Main(string[] args) {
+            Songs[0] = new Song("リリック", " TOKIO", 72);
+
+            PrintSongs(Songs);
+           
+        }
+        private static void PrintSongs(Song[] songs) {
+            int min = Songs.Length / 60;
+            int sec = Songs.Length % 60;
+            foreach (var song in songs) {
+                Console.WriteLine("{0},{1},{2}:{3}", songs[0].Title, songs[0].ArtistName, min, sec);
+            }
         }
     }
 }
---
+++ Exercise1/Song.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1 {
+    class Song {
+        public String Title { get; set; }
+
+        public string ArtistName { get; set; }
+
+        public int Length { get; set; }
+
+
+        public Song(String Title,String ArtistName,int Length) {
+            this.Title = Title;
+            this.ArtistName = ArtistName;
+            this.Length = Length;
+
+
+        }
+        }
+
+
+
+    }
+
