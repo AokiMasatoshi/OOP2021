96980cbf145b47f235e98c8d0d114a9d3702cd7c
青木 将隼 <32145@ojs.ac.jp>
2021-06-01 09:42:52 +0900

    P79 問題2.2完成

---
+++ Exercise2/InchConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2 {
+   public static class InchConverter {
+
+        private const double ratio = 0.0254;
+
+        public static double FromMeter(double meter) {
+            return meter / ratio;
+        }
+
+        //フィートからメートルを求める
+        public static double ToMeter(double Inch) {
+            return Inch * ratio;
+        }
+
+
+
+    }
+}
---
+++ Exercise2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2 {
+    class Program {
+        static void Main(string[] args) {
+            //フィートからメートルへの対応表を出力
+                PrintInchToMeterList(1, 10);
+        }
+
+
+
+        private static void PrintInchToMeterList(int start, int stop) {
+            for (int Inch = start; Inch <= stop; Inch++) {
+                double meter = InchConverter.ToMeter(Inch);
+                Console.WriteLine("{0}Inch = {1:0.0000}m", Inch, meter);
+            }
+        }
+    }
+}
+
