df400d07b927baa726d882e0b8e607633e9b3d0c
青木 将隼 <32145@ojs.ac.jp>
2021-06-01 10:14:23 +0900

    P79 問題2.3　

---
+++ Exercise3/SalesCounter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesCalculator {
+    //売り上げ集計クラス
+    class SalesCounter {
+
+        private IEnumerable<Sale> _sales;
+
+        public SalesCounter(string filePath){
+            _sales = ReadSales(filePath);
+
+        }
+
+        // List 2-15
+        // 売り上げデータを読み込み、Saleオブジェクトのリストを返す
+        private static IEnumerable<Sale> ReadSales(string filePath) {
+            List<Sale> sales = new List<Sale>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines) {
+                string[] items = line.Split(',');
+                Sale sale = new Sale {
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2])
+                };
+                sales.Add(sale);
+            }
+            return sales;
+        }
+
+        public IDictionary<string, int> GetPerStoreSales(){
+            var dict = new Dictionary<string, int>();
+            foreach(var sale in _sales) {
+                if (dict.ContainsKey(sale.ProductCategory)) 
+                    //すでにコレクションに店舗が設定されている
+                    dict[sale.ProductCategory] += sale.Amount;
+                else 
+                    //コレクションへ店舗を登録
+                    dict[sale.ProductCategory] = sale.Amount;
+                }
+                return dict;
+            
+        }
+    }
+}
