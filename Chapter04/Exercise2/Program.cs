a020b2ce8bb5697002e80987a8116384f84964b7
青木 将隼 <32145@ojs.ac.jp>
2021-06-11 15:22:13 +0900

    P134 問題4.2 完成

---
+++ Chapter04/Exercise2/Program.cs
@@ -0,0 +1,70 @@
+﻿using Exercise1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2 {
+    class Program {
+        static void Main(string[] args) {
+            var ymCollection = new YearMonth[5] {
+            new YearMonth (1999, 1),
+            new YearMonth(2001, 5),
+            new YearMonth(2004, 7),
+            new YearMonth(2009, 9),
+            new YearMonth(2015, 12),
+        };
+            Exercise2_2(ymCollection);
+            Console.WriteLine("-----");
+
+            Exercise2_4(ymCollection);
+            Console.WriteLine("-----");
+
+            Exercise2_5(ymCollection);
+            Console.WriteLine("-----");
+
+        }
+
+        private static void Exercise2_2(YearMonth[] ymCollection) {
+
+            foreach (var ym in ymCollection) {
+                Console.WriteLine(ym);
+
+            }
+        }
+
+         static YearMonth FindFirst21C(YearMonth[] ymCollection) {
+            foreach (var ym in ymCollection) {
+                if (ym.Is21Cevtury) {
+                    return ym;
+                }
+            }
+            return null;
+        }
+
+
+        private static void Exercise2_4(YearMonth[] ymCollection) {
+            var ymCheck = FindFirst21C(ymCollection);
+            //if (ymCheck!= null) {
+            //    Console.WriteLine(ymCheck.Year);
+            //} else {
+            //    Console.WriteLine("21世紀のデータはありません");
+            //}
+            var s = ymCheck != null ? ymCheck.Year.ToString() : "21世紀のデータはありません";
+            Console.WriteLine(s);
+        }
+
+        private static void Exercise2_5(YearMonth[] ymCollection) {
+            var array = ymCollection.Select(ym=>ym.AddOneMonth()).ToArray();
+            foreach (var ym in array) {
+                Console.WriteLine(ym);
+
+            }
+
+
+
+        }
+    }
+}
+
--- Chapter04/Exercise2/Properties/AssemblyInfo.cs
+++ Chapter04/Exercise2/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // 制御されます。アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更します。
-[assembly: AssemblyTitle("Exercie2")]
+[assembly: AssemblyTitle("Exercise2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Exercie2")]
+[assembly: AssemblyProduct("Exercise2")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
-[assembly: Guid("bccb8ff6-74f5-4e24-ac74-2c50a0eafb7e")]
+[assembly: Guid("f6401327-f54a-4739-94fa-68330ccdbf32")]
 
 // アセンブリのバージョン情報は次の 4 つの値で構成されています:
 //
