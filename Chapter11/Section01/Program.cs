63a6263987f411376f5a3c33d0c0598cb94cf3d9
将隼 青木 <32145@ojs.ac.jp>
2021-08-26 15:39:41 +0900

    リスト11.7まで

---
+++ Chapter11/Section01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Section01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var xdox = XDocument.Load("novelists.xml");
+            //var xelements = xdox.Root.Elements()
+            //    .Where(x => ((DateTime)x.Element("birth")).Year >= 1900);
+            //var xelements = xdox.Root.Elements()
+            //    .OrderBy(x => (string)(x.Element("birth")));
+
+
+            foreach (var xnovelist in xdox.Root.Elements())
+            {
+                var xname = xnovelist.Element("name");
+                var works = xnovelist.Element("masterpieces")
+                                     .Elements("title")
+                                     .Select(x=>x.Value);
+                //XAttribute xkana = xname.Attribute("kana");
+                //var xbirth= (DateTime)xnovelist.Element("birth");
+                Console.WriteLine("{0} - {1}" ,xname.Value,string.Join(",",works));
+
+            }
+        }
+    }
+}
