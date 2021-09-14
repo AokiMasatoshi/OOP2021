150994fed6777bc217e1793d1b3625355cf6b572
将隼 青木 <32145@ojs.ac.jp>
2021-09-14 14:04:05 +0900

    ⑤途中

--- RssReader/RssReader/Form2.Designer.cs
+++ RssReader/RssReader/Form2.Designer.cs
@@ -30,8 +30,8 @@ namespace RssReader
         private void InitializeComponent()
         {
             this.wbBrowser2 = new System.Windows.Forms.WebBrowser();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btMoveOn = new System.Windows.Forms.Button();
+            this.btReturn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // wbBrowser2
@@ -40,32 +40,35 @@ namespace RssReader
             this.wbBrowser2.MinimumSize = new System.Drawing.Size(20, 20);
             this.wbBrowser2.Name = "wbBrowser2";
             this.wbBrowser2.Size = new System.Drawing.Size(795, 386);
-            this.wbBrowser2.TabIndex = 0;             
-            // button1
+            this.wbBrowser2.TabIndex = 0;
             // 
-            this.button1.Location = new System.Drawing.Point(172, 8);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(128, 44);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "進む";
-            this.button1.UseVisualStyleBackColor = true;
+            // btMoveOn
             // 
-            // button2
+            this.btMoveOn.Location = new System.Drawing.Point(172, 8);
+            this.btMoveOn.Name = "btMoveOn";
+            this.btMoveOn.Size = new System.Drawing.Size(128, 44);
+            this.btMoveOn.TabIndex = 1;
+            this.btMoveOn.Text = "進む";
+            this.btMoveOn.UseVisualStyleBackColor = true;
+            this.btMoveOn.Click += new System.EventHandler(this.btMoveOn_Click);
             // 
-            this.button2.Location = new System.Drawing.Point(12, 8);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(128, 44);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "戻る";
-            this.button2.UseVisualStyleBackColor = true;
+            // btReturn
+            // 
+            this.btReturn.Location = new System.Drawing.Point(12, 8);
+            this.btReturn.Name = "btReturn";
+            this.btReturn.Size = new System.Drawing.Size(128, 44);
+            this.btReturn.TabIndex = 1;
+            this.btReturn.Text = "戻る";
+            this.btReturn.UseVisualStyleBackColor = true;
+            this.btReturn.Click += new System.EventHandler(this.btReturn_Click);
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btReturn);
+            this.Controls.Add(this.btMoveOn);
             this.Controls.Add(this.wbBrowser2);
             this.Name = "Form2";
             this.Text = "Form2";
@@ -75,8 +78,8 @@ namespace RssReader
         }
 
         #endregion
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btMoveOn;
+        private System.Windows.Forms.Button btReturn;
         private System.Windows.Forms.WebBrowser wbBrowser2;
     }
 }
--- RssReader/RssReader/Form2.cs
+++ RssReader/RssReader/Form2.cs
@@ -13,17 +13,40 @@ namespace RssReader
     public partial class Form2 : Form
     {
         Uri urlCode;
+        List<Uri> urls= new List<Uri>();
 
         public Form2(string url)
         {
             InitializeComponent();
-            this.urlCode = new Uri(url); 
+            this.urlCode = new Uri(url);
+            btReturn.Enabled = false;
+            btMoveOn.Enabled = false;
         }
 
         private void Form2_Load(object sender, EventArgs e)
         {
             wbBrowser2.Url = urlCode;
+            urls.Add(wbBrowser2.Url);
         }
 
+
+        private void btReturn_Click(object sender, EventArgs e)
+        {
+
+            if (urls[0] != wbBrowser2.Url)
+            {
+               btReturn.Enabled = true;
+               
+            }
+        }
+
+        private void btMoveOn_Click(object sender, EventArgs e)
+        {
+            //if ()
+            //{
+
+            //}
+
+        }
     }
 }
